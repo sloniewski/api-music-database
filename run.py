113cdc7f0dd544b9eb51cecc3328aa61a10fa7b1
sloniewski <sloniewski.maciej@gmail.com>
2018-06-21 00:05:34 +0200

    support for running app with different configs

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 from app import create_app
+import argparse
 
+def set_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config",
+                        action="store", dest="config",
+                        help="Config name")
+    options = parser.parse_args()
+    return options
 
-app = create_app('development')
 
 if __name__ == '__main__':
+    
+    options = set_options()
+    config_name = 'development'
+    if options.config:
+        config_name = options.config
+
+    app = create_app(config_name)
     app.run()
