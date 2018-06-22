f102c4f0069f467b2c223e4c915c2a1739e168b9
sloniewski.maciej <sloniewski.maciej@gmail.com>
2018-06-22 04:59:39 -0400

    prepared band views

--- app/band/views.py
+++ app/band/views.py
@@ -8,44 +8,34 @@ from .models import Band
 
 @band.route('/<int:id>', methods=['GET'])
 @apply_media_type(request=request)
-def get_band(id):
+def band_get(id):
     band = Band.query.get_or_404(id)
-    return {'name': band.name }
+    return Band.as_dict()
 
-@band.route('/<string:slug>', methods=['PUT', 'DELETE', 'PATCH'])
+
+@band.route('/<int:id>', methods=['DELETE'])
 @token_required(request=request)
-def band_handler(slug):
+def band_delete(slug):
+    return '!'
 
-    def put():
-        pass
 
-    def patch():
-        pass
+@band.route('/<int:id>', methods=['PUT'])
+@token_required(request=request)
+def band_put(slug):
+    return '!'
 
-    def delete(req, slug):
-        return '!'
 
-    handlers = {
-        'put': put,
-        'patch': patch,
-        'delete': delete,
-    }  
-    return handlers[request.method.lower()](req=request, slug=slug)
+@band.route('/<int:id>', methods=['PATCH'])
+@token_required(request=request)
+def band_patch(slug):
+    return '!'
+
 
 
 @band.route('/', methods=['GET'])
 @apply_media_type(request=request)
 def get_bands():
-    data = {
-        'records': 2,
-        'next_page': 'page_url/?page=3',
-        'prev_page': 'page_url/?page=1',
-        'results': [
-            {'name': 'Foo fighters' },
-            {'name': 'Stereophonics' } ,
-        ]
-    }
-    return data
+    return '!'
 
 
 @band.route('/', methods=['POST'])
