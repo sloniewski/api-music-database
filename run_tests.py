27918778f44440c97d78811a983b246e895dfd22
sloniewski.maciej <sloniewski.maciej@gmail.com>
2018-06-27 07:26:49 -0400

    more serializer tests

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from tests.tests_serializer import *
 from tests.tests_auth import *
 from tests.tests_band import *
 
--- tests/tests_serializer.py
+++ tests/tests_serializer.py
@@ -1,6 +1,9 @@
 import unittest
 
+from werkzeug.exceptions import UnsupportedMediaType
+
 from app.main.serializer import XmlSerializer, JsonSerializer
+
 
 class TestSerializers(unittest.TestCase):
     
@@ -15,17 +18,35 @@ class TestSerializers(unittest.TestCase):
                 
     def test_xml_to_dict(self):
         serializer = XmlSerializer()
-        data = '{"name":"John", "age":30, "car":null}' #json
+        data = '''<?xml version="1.0" encoding="UTF-8"?>
+                    <note>
+                      <to>Tove</to>
+                      <from>Jani</from>
+                      <heading>Reminder</heading>
+                      <body>Don't forget me this weekend!</body>
+                    </note>'''
         result = serializer.deserialize(data)
         self.assertIsInstance(result, dict)
         self.assertEqual(['a','b','c'], result.keys())
     
     def test_fail_dict_to_xml(self):
-        pass
+        serializer = XmlSerializer()
+        data = {'a': 1, 'b': 'abc', 'c': [1, 2, 'x']}
+        with self.assertRaises(UnsupportedMediaType):
+            result = serializer.serialize(data)
     
     def test_fail_xml_to_dict(self):
-        pass
-    
+        serializer = XmlSerializer()
+        data = '''<?xml version="1.0" encoding="UTF-8"?>
+                <note>
+                  <to>Tove</to>
+                  <from>Jani</from>
+                  <heading>Reminder</heading>
+                  <body>Don't forget me this weekend!</body>
+                </note>'''
+        with self.assertRaises(UnsupportedMediaType):
+            result = serializer.deserialize(data)
+           
     def test_dict_to_json(self):
         serializer = JsonSerializer()
         data = {'a': 1, 'b': 'abc', 'c': [1, 2, 'x']}
@@ -39,8 +60,15 @@ class TestSerializers(unittest.TestCase):
         self.assertIsInstance(result, dict)
         self.assertEqual(['a','b','c'], result.keys())
     
-    def test_fail_dict_to_xml(self):
-        pass
-    
-    def test_fail_xml_to_dict(self):
-        pass
+    def test_fail_dict_to_json(self):
+        serializer = JsonSerializer()
+        data = {'a': 1, 'b': 'abc', 'c': [1, 2, 'x']}
+        with self.assertRaises(UnsupportedMediaType):
+            result = serializer.serialize(data)
+        
+    def test_fail_json_to_dict(self):
+        serializer = JsonSerializer()
+        data = '{"name":"John", "age":30, "car":null}' #json
+        with self.assertRaises(UnsupportedMediaType):
+            result = serializer.deserialize(data)
+           
